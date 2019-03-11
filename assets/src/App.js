fa1144ffbd661a6de2603b4f752d0ea69ebec20c
Maks Yusanenko <flashmaks7@gmail.com>
2019-03-11 23:48:52 +0200

    добавили пример структуры компонентов.

---
+++ assets/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./components/WWW"
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello 12312434</div>
+    );
+  }
+}
+
+App.propTypes = {};
+
+export default App;
