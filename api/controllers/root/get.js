5ce10b5017bca7d324007e857194ab1d532533a4
Sergey Panchenko <serzhik.panchenko@gmail.com>
2019-03-12 01:05:25 +0200

    fixed async and action examples

---
+++ api/controllers/root/get.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Get',
+
+
+  description: 'Get root.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+
+    return {message: 'THIS IS TITLE!', body: 'body', items: [1,2,'string1']};
+
+  }
+
+
+};
--- assets/src/components/WWW/index.js
+++ assets/src/components/WWW/index.js
@@ -1,1 +1,27 @@
 import "./index.scss"
+import React, {Component} from 'react';
+
+
+class WWW extends Component {
+state = {message:'', body:''}
+  async componentDidMount() {
+    const response =  await fetch('http://localhost:1337/api/v1/root/')
+    const data = await response.json()
+    console.log(data)
+
+    this.setState(data);
+
+  }
+
+  render() {
+  const{message, body} = this.state
+    return (
+      <div>
+        {message}
+        {body}
+      </div>
+    );
+  }
+}
+
+export default WWW;
