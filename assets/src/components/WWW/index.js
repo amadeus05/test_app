09fdcb9a54401ba0d86b5317daa35040587c88e0
Sergey Panchenko <serzhik.panchenko@gmail.com>
2019-04-10 00:11:25 +0200

    eslint fixes

--- assets/src/components/WWW/index.js
+++ assets/src/components/WWW/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 
-
 class WWW extends Component {
-state = {message:'', body:''}
+  state = {message:'', body:''}
+
   async componentDidMount() {
     const response =  await fetch('http://localhost:1337/api/v1/root/')
     const data = await response.json()
     console.log(data)
-
     this.setState(data);
-
   }
 
   render() {
-  const{message, body} = this.state
+    const{message, body} = this.state
     return (
       <div>
         {message}
